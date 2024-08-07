e03a2a78da58e68ea487632dbcd6d2bbe592596d
devprinceng <devprinceng@gmail.com>
2024-08-07 15:52:33 +0100

    modified app, added authRouter

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const dotenv = require('dotenv')
 dotenv.config();
-
+const { authRouter } = require('./routes')
 const connectMongoDb = require('./init/MongoDB');
 
 //connect database
@@ -14,5 +14,8 @@ const app = express()
 //use thirdparty middlewares
 app.use(express.json({limit: '500mb'}));
 app.use(bodyParser.urlencoded({limit:'500mb',extended:true}));
+
+//routes
+app.use('/api/v1/auth/', authRouter)
 
 module.exports = app;
