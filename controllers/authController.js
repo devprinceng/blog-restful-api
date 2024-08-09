c2dd6eb7cb5f054430f79802b6675344bfaab4da
devprinceng <devprinceng@gmail.com>
2024-08-09 06:49:30 +0100

    added validation to register fields

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,25 @@ const { User } = require("../models");
 const register = async (req, res, next) => {
     try {
         const { name, email, password, role } = req.body;
+        
+        //validations
+        if(!name){
+            res.code = 400;
+            throw new Error('name is required');
+        }
+        if(!email){
+            res.code = 400;
+            throw new Error('email is required');
+        }
+        if(!password){
+            res.code = 400;
+            throw new Error('password is required');
+        }
+        if(password.length < 6){
+            res.code = 400;
+            throw new Error('password must be greater than 6 characters');
+        }
+
         const newUser = await User({name, email, password, role});
 
         await newUser.save();
