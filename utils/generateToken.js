a69e05feaf6d951f430b833a377b970dda75698d
devprinceng <devprinceng@gmail.com>
2024-08-14 07:16:49 +0100

    added generateToken functionality to utils

---
+++ utils/generateToken.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+const { jwtSecretKey } = require('../config/keys');
+
+const generateToken = (user) => {
+
+   const token =  jwt.sign(
+        {
+            _id: user._id,
+            email:user.email},
+            jwtSecretKey,
+            {
+                expiresIn: '7d'
+            }
+        )
+        
+    return token;
+}
+
+module.exports = generateToken
