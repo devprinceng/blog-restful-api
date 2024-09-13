ae8ec122afb89b782bad34d25ed42b1af030ecee
devprinceng <devprinceng@gmail.com>
2024-09-13 23:44:38 +0100

    added email smtp credentials

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,10 @@
-const {PORT, CONNECTION_URL, JWT_SECRET_KEY} = process.env
+const { PORT, CONNECTION_URL, JWT_SECRET_KEY, EMAIL_SENDER, EMAIL_PASSWORD } =
+  process.env;
 
 module.exports = {
-    port:PORT,
-    connectionUrl:CONNECTION_URL,
-    jwtSecretKey:JWT_SECRET_KEY,
-}
+  port: PORT,
+  connectionUrl: CONNECTION_URL,
+  jwtSecretKey: JWT_SECRET_KEY,
+  email_user: EMAIL_SENDER,
+  email_pass: EMAIL_PASSWORD,
+};
