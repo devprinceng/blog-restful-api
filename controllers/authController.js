1456f31e589c7fa9f22386ec50781afe2b198f99
devprinceng <devprinceng@gmail.com>
2024-09-13 23:50:46 +0100

    completed send email verification code functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,128 +1,137 @@
 const { User } = require("../models");
-const hashPassword = require('../utils/hashedPassword')
-const comparePassword = require('../utils/comparePassword');
+const hashPassword = require("../utils/hashedPassword");
+const comparePassword = require("../utils/comparePassword");
 const generateToken = require("../utils/generateToken");
-const generateCode = require('../utils/generateCode');
+const generateCode = require("../utils/generateCode");
+const sendEMailVerification = require("../utils/sendEmailVerification");
+
+//register controller
 const register = async (req, res, next) => {
-    try {
-        const { name, email, password, password_confirm, role } = req.body;
-        
-        //validations
-        // if(!name){
-        //     res.code = 400;
-        //     throw new Error('name is required');
-        // }
-        // if(!email){
-        //     res.code = 400;
-        //     throw new Error('email is required');
-        // }
-        // if(!password){
-        //     res.code = 400;
-        //     throw new Error('password is required');
-        // }
-        // if(password.length < 6){
-        //     res.code = 400;
-        //     throw new Error('password must be greater than 6 characters');
-        // }
-        // if(password !== password_confirm){
-        //     res.code = 400;
-        //     throw new Error('password confirmation does not match');
-        // }
-        //! since we're using express validator no need keeping the above validations
+  try {
+    const { name, email, password, password_confirm, role } = req.body;
 
-        //* lets check if email exist on DB
-        const isEmailExist = await User.findOne({ email });
-        if(isEmailExist){
-            res.status(400);
-            throw new Error("Email already exist");
-        }
+    //validations
+    // if(!name){
+    //     res.code = 400;
+    //     throw new Error('name is required');
+    // }
+    // if(!email){
+    //     res.code = 400;
+    //     throw new Error('email is required');
+    // }
+    // if(!password){
+    //     res.code = 400;
+    //     throw new Error('password is required');
+    // }
+    // if(password.length < 6){
+    //     res.code = 400;
+    //     throw new Error('password must be greater than 6 characters');
+    // }
+    // if(password !== password_confirm){
+    //     res.code = 400;
+    //     throw new Error('password confirmation does not match');
+    // }
+    //! since we're using express validator no need keeping the above validations
 
-        //hash password
-        const password_hash = await hashPassword(password);
-        console.log(password_hash);
-        //create new user
-        const newUser = await User({name, email, password:password_hash, role});
-
-        await newUser.save();
-        // send response
-        return res.status(201).json({code: 201, status: true, message: "User Created Successul", data: newUser})
-    } catch (error) {
-        next(error)
+    //* lets check if email exist on DB
+    const isEmailExist = await User.findOne({ email });
+    if (isEmailExist) {
+      res.status(400);
+      throw new Error("Email already exist");
     }
-}
+
+    //hash password
+    const password_hash = await hashPassword(password);
+    // console.log(password_hash);
+    //create new user
+    const newUser = await User({ name, email, password: password_hash, role });
+
+    await newUser.save();
+    // send response
+    return res.status(201).json({
+      code: 201,
+      status: true,
+      message: "User Created Successul",
+      data: newUser,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 
 const login = async (req, res, next) => {
-    try {
-        const {email, password } = req.body;
+  try {
+    const { email, password } = req.body;
 
-        //check if username exist
-        const user = await User.findOne({email});
-        if(!user){
-            res.status(401);
-            throw new Error('Invalidate email or password');
-        }
-
-        //check password match
-        const matched = await comparePassword(password, user.password)
-
-        //compare password
-        if(!matched){
-            res.status(401);
-            throw new Error('Invalidate email or password');
-        }
-        //token
-        const token = generateToken(user);
-
-        return res
-                .status(200)
-                .json(
-                    {
-                        code: 200,
-                        status: true,
-                        message: "Login Successful",
-                        data: {token}
-                    })
-    } catch (error) {
-        next(error);
+    //check if username exist
+    const user = await User.findOne({ email });
+    if (!user) {
+      res.status(401);
+      throw new Error("Invalid email or password");
     }
-    
-}
+
+    //check password match
+    const matched = await comparePassword(password, user.password);
+
+    //compare password
+    if (!matched) {
+      res.status(401);
+      throw new Error("Invalid email or password");
+    }
+    //token
+    const token = generateToken(user);
+
+    return res.status(200).json({
+      code: 200,
+      status: true,
+      message: "Login Successful",
+      data: { token },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 
 //verifyemail controller
 const verifyCode = async (req, res, next) => {
-    try {
-        const { email } = req.body;
+  try {
+    const { email } = req.body;
 
-        //find user
-        const user = await User.findOne({email});
-        if(!user){
-            res.status(404);
-            throw new Error('User not Found');
-        }
-
-        //check if user is verified
-        if(user.isVerified){
-            res.status(400);
-            throw new Error('User already verified');
-        }
-
-        const code = generateCode(6);
-
-        user.verificationCode = code;
-        await user.save();
-
-        // send email to user
-
-        res
-            .status(200)
-            .json({
-                code: 200,
-                status: true,
-                message: "User successfully verified"
-            });
-    } catch (error) {
-        next(error)
+    //find user
+    const user = await User.findOne({ email });
+    if (!user) {
+      res.code = 404;
+      throw new Error("User not Found");
     }
-}
 
-module.exports = { register, login, verifyCode }
+    //check if user is verified
+    if (user.isVerified) {
+      res.code = 400;
+      throw new Error("User already verified");
+    }
+
+    const code = generateCode(6);
+
+    //set the verification code of the user in db
+    user.verificationCode = code;
+    await user.save();
+
+    // send email to user
+    await sendEMailVerification({
+      emailTo: user.email,
+      subject: "Verification Code",
+      code,
+      content: "verify your new account",
+    });
+
+    res.status(200).json({
+      code: 200,
+      status: true,
+      message: "Verification Code sent successfully",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { register, login, verifyCode };
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,13 +1,18 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { authController } = require('../controllers/')
-const { registerValidator } = require('../validators/auth')
-const { loginValidator } = require('../validators/auth')
-const { emailValidator } = require('../validators/auth')
-const validate = require('../validators/validate')
+const { authController } = require("../controllers/");
+const { registerValidator } = require("../validators/auth");
+const { loginValidator } = require("../validators/auth");
+const { emailValidator } = require("../validators/auth");
+const validate = require("../validators/validate");
 
-router.post('/register', registerValidator, validate, authController.register)
-router.post('/login', loginValidator, validate, authController.login)
-router.post('/send-verification-code', emailValidator, validate, authController.verifyCode)
+router.post("/register", registerValidator, validate, authController.register);
+router.post("/login", loginValidator, validate, authController.login);
+router.post(
+  "/send-verification-code",
+  emailValidator,
+  validate,
+  authController.verifyCode
+);
 
-module.exports = router; 
+module.exports = router;
